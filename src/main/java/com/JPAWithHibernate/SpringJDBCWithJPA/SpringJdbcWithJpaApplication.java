36481590455e39e683f469c0a91ab34b79c4e3ad
nishank1.soni <nishank1.soni@ril.com>
2019-06-16 07:53:18 +0530

    Spring Data JPA

---
+++ src/main/java/com/JPAWithHibernate/SpringJDBCWithJPA/SpringJdbcWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.JPAWithHibernate.SpringJDBCWithJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcWithJpaApplication.class, args);
+	}
+
+}
